18c6e4ececce54dc6cb24e24bbf3b9c419943966
hailey.forar <hailey.forar@MU-C1-20.mullen.pvt.k12.co.us>
2019-08-22 11:55:32 -0600

    Second commit - left off figuring out the initial rolls

--- Craps Game/src/CrapsGame.java
+++ Craps Game/src/CrapsGame.java
@@ -17,7 +17,19 @@ public class CrapsGame
 			String name = userInput.nextLine();
 			System.out.println("Hello " + name + " Let's play a game of Craps.");
 			
-			int randomRoll = (int) (Math.random() * 7 + 1);
+			int dice1 = (int) (Math.random() * 7 + 1);
+			int dice2 = (int) (Math.random() * 7 + 1);
+			int totalDice = dice1 + dice2;
+			
+			if(totalDice == 7 || totalDice == 11)
+				{
+					System.out.println("You rolled a " + dice1 + " and a " + dice2 + " for a total of 7. You win, " + name + "!");
+				}
+			
+			else if(totalDice == 2 || totalDice == 12)
+				{
+					System.out.println("You rolled a " + dice1 + " and a" + dice2 + " for a total of " + totalDice + ". You win, " + name + "!" );
+				}
 		}
 		
 		public static void part2()
